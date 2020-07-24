d2b4f5776b31c1ac35ac5e01a21037baebcd50c6
ramirezshirleym <67880951+ramirezshirleym@users.noreply.github.com>
2020-07-23 23:33:17 -0400

    Complete Excercise 4

--- Box/src/Box.java
+++ Box/src/Box.java
@@ -1,9 +1,31 @@
 
 public class Box {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Declare instance variable with default value
+	public int size = 0;
 
+	public static void main(String[] args) {
+
+		// Create new Box object
+		Box starBox = new Box();
+
+		// Change size to 5 for starBox object
+		starBox.size = 5;
+
+		// Invoke printBox method
+		starBox.printBox();
+	}
+
+	// Define printBox instance method
+	public void printBox() {
+		for (int row = 1; row <= size; row++) {
+
+			for (int col = 1; col <= size; col++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
 	}
 
 }
